0ed5461c71a07b98b8bcdc087be6364bbfdbd98d
lsb0225 <leesungbin0415@gmail.com>
2018-04-13 15:00:54 +0900

    Initial commit

---
+++ app/src/main/java/edu/android/gitproject/MainActivity.java
@@ -0,0 +1,20 @@
+package edu.android.gitproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        textView.setText("안녕하세요!");
+    }
+}
